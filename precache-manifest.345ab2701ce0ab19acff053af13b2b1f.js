62ed3ea3e9eac45c5a857e935c188ab3eea0fdc8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-06 07:06:20 +0000

    Deploy okwast/PlanningPoker to github.com/okwast/PlanningPoker.git:gh-pages

---
+++ precache-manifest.345ab2701ce0ab19acff053af13b2b1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c715a9d8fec706413d764f8035d3a90",
+    "url": "/PlanningPoker/index.html"
+  },
+  {
+    "revision": "096fe82bf1d8c4c19502",
+    "url": "/PlanningPoker/static/css/main.0035b791.chunk.css"
+  },
+  {
+    "revision": "83a842911660868ae05c",
+    "url": "/PlanningPoker/static/js/2.75d1328e.chunk.js"
+  },
+  {
+    "revision": "096fe82bf1d8c4c19502",
+    "url": "/PlanningPoker/static/js/main.87b338e6.chunk.js"
+  },
+  {
+    "revision": "f23f18132f6ba0a96efe",
+    "url": "/PlanningPoker/static/js/runtime-main.eac19345.js"
+  }
+]);
